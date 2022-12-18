6e89d79e8df3e95798f6b3fd1eec860033efdc51
no-yan <okamoto-wataru@dmm.com>
2022-12-18 17:57:12 +0900

    add some code

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,1 +1,5 @@
+import {validateEmail } from "server";
+
 export type ClientType = 'WEB';
+
+validateEmail("hogehoge.email");
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,1 +1,5 @@
-export type DefaultPath = 'https://localhost:8000/'
+export const emailRegex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+
+function validateEmail(email: string) {
+	return emailRegex.test(email);
+}
